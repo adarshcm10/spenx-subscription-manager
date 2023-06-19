1ac78669f3b0ea5cd643377cb792090585d84dc7
Adarsh C M <11200049@rist.edu.in>
2023-06-19 19:38:06 +0530

    datepicker only accept present future date

--- lib/datepick.dart
+++ lib/datepick.dart
@@ -19,7 +19,7 @@ class _YourWidgetState extends State<YourWidget> {
     final DateTime? picked = await showDatePicker(
         context: context,
         initialDate: DateTime.now(),
-        firstDate: DateTime(2000),
+        firstDate: DateTime.now(),
         lastDate: DateTime(2100),
         builder: (context, child) {
           return Theme(
